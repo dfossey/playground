dbeb214ee44e116ce485b3de17b80fef1ef3953d
Damien FOSSEY <fossey.d@outlook.com>
2025-08-11 19:41:01 +0200

    add option to trigger dropdown on hover or click

--- resources/views/components/plugins/dropdown.blade.php
+++ resources/views/components/plugins/dropdown.blade.php
@@ -1,35 +1,41 @@
 @props([
     'btnClass' => '',
     'dropdownClass' => 'bg-light-secondary',
+    'hover' => true,
 ])
 
 <div
     class="dropdown {{ $attributes['class'] ?? '' }}"
-    x-data="{
-        show: false,
-        hideTimer: null
-    }"
-    x-on:mouseenter="show = true"
-    x-on:mouseleave="show = false"
-    x-init="
-        $watch('show', (value) => {
-            const dropdown = bootstrap.Dropdown.getOrCreateInstance($el);
-            clearTimeout($data.hideTimer);
+    @if ($hover)
+        x-data="{
+            show: false,
+            hideTimer: null
+        }"
+        x-on:mouseenter="show = true"
+        x-on:mouseleave="show = false"
+        x-init="
+            $watch('show', (value) => {
+                const dropdown = bootstrap.Dropdown.getOrCreateInstance($el);
+                clearTimeout($data.hideTimer);
 
-            if (value) {
-                dropdown.show();
-            } else {
-                $data.hideTimer = setTimeout(() => dropdown.hide(), 100);
-            }
-        })
-    "
+                if (value) {
+                    dropdown.show();
+                } else {
+                    $data.hideTimer = setTimeout(() => dropdown.hide(), 100);
+                }
+            })
+        "
+    @endif
 >
     <button
         class="btn {{ $btnClass }}"
         type="button"
-        {{-- data-bs-toggle="dropdown" --}}
-        {{-- aria-expanded="false" --}}
-        x-bind:class="{ 'showed': show }"
+        @if ($hover)
+            x-bind:class="{ 'showed': show }"
+        @else
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+        @endif
     >
         {{ $label ?? $attributes['label'] ?? '' }}
     </button>
